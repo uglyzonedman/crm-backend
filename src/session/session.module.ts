954d726b3e9852192b456867d98f2fb7af97a5ea
-_- <majest228@gmail.com>
2025-07-02 18:29:12 +0500

    Добавление jwt токенов

--- src/session/session.module.ts
+++ src/session/session.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SessionService } from './session.service';
 import { SessionController } from './session.controller';
 import { PrismaService } from 'src/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [SessionController],
-  providers: [SessionService, PrismaService],
+  providers: [SessionService, PrismaService, JwtService],
 })
 export class SessionModule {}
--- src/session/session.service.ts
+++ src/session/session.service.ts
@@ -1,11 +1,20 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  BadGatewayException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { AddSessionDto } from './session.dto';
 import { Prisma } from 'generated/prisma';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class SessionService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async addSession(dto: AddSessionDto, req: any) {
     try {
@@ -40,5 +49,62 @@ export class SessionService {
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
+  }
+
+  async updateSession(refreshToken: string, req: any) {
+    const currentRefreshToken = await this.prisma.sessionUser.findFirst({
+      where: {
+        refreshToken: refreshToken,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    if (!currentRefreshToken) throw new BadGatewayException('..');
+
+    await this.prisma.sessionUser.update({
+      where: {
+        id: currentRefreshToken.id,
+      },
+      data: {
+        isRevoked: true,
+      },
+    });
+
+    const payload = {
+      sub: currentRefreshToken.user.id,
+      login: currentRefreshToken.user.login,
+    };
+    const now = Date.now();
+    const newAccessTokenExpiresInMs = 45 * 60 * 1000; // 45 минут
+    const newRefreshTokenExpiresInMs = 7 * 24 * 60 * 60 * 1000; // 7 дней
+
+    const newAccessToken = this.jwtService.sign(payload, { expiresIn: '45m' });
+    const newRefreshToken = this.jwtService.sign(payload, { expiresIn: '7d' });
+
+    const newSession = await this.prisma.sessionUser.create({
+      data: {
+        refreshToken: newRefreshToken,
+        expiresAt: new Date(now + newRefreshTokenExpiresInMs).toISOString(),
+        ip: req.ip || '',
+        userAgent: req.headers['user-agent'] || '',
+        isRevoked: false,
+        userId: currentRefreshToken.user.id,
+      },
+    });
+
+    const response = {
+      ...newSession,
+      accessToken: newAccessToken,
+      accessTokenExpiresInMs: newAccessTokenExpiresInMs,
+      refreshToken: newRefreshToken,
+    };
+
+    return {
+      status: 'success',
+      message: 'done',
+      data: response,
+    };
   }
 }
